0088ffdf0854cbb8c5e915e9525d23dc06902e2b
Bekbolat Kurmetbek <59786257+Bekbo01@users.noreply.github.com>
2024-07-05 15:12:06 +0500

    Update settings.py

--- myelearning/settings.py
+++ myelearning/settings.py
@@ -103,10 +103,17 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'myelearning', 
+        'USER': 'myelearning', 
+        'PASSWORD': 'myelearning',
+        'PORT': '5432', 
+    } 
 }
 
 
